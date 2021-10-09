38dbf630555e0759b6a23f3fd44bd724b6656197
My AnFeVaGa <Job.andresvalencia@gmail.com>
2021-10-08 22:21:34 -0500

    School menu option two

--- src/School.java
+++ src/School.java
@@ -1,9 +1,13 @@
+import com.sun.org.apache.xerces.internal.xs.StringList;
+
+import javax.security.auth.Subject;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class School {
 
     static ArrayList<Student> students = new ArrayList<>();
+
 
     public static void main(String[] args) {schoolArray();}
     public static void schoolArray() {
@@ -14,9 +18,13 @@ public class School {
             if (menuInput == 1) {
                 showOptionOne();
             }
+            if (menuInput == 2){
+                showOptionTwo();
+            }
 
         }while(true);
     }
+
     private static int showMainMenu() {
         int menuInput;
         do {
@@ -36,16 +44,72 @@ public class School {
         String studentName = inputStudent.nextLine();
 
         System.out.println("Grade to which the student enters " + (students.size() + 1) + ": ");
+        ArrayList<String> grade = new ArrayList<>();
+        grade.add("Sixth");
+        grade.add("Seventh");
+        grade.add("Eighth");
+        grade.add("Ninth");
+        grade.add("Tenth");
+        grade.add("Eleven");
+        System.out.println(grade);
         String studentGrade = inputStudent.nextLine();
 
-        System.out.println("Subjects to be registered " + (students.size() + 1) + ": ");
+        System.out.println("Subjects to be registered, minimum one to maximum three " + (students.size() + 1) + ": ");
+
+        ArrayList<String> subjects = new ArrayList<>();
+        subjects.add("Mathematics\n");
+        subjects.add("Spanish\n");
+        subjects.add("Science\n");
+        System.out.println(subjects);
         String studentSubjects = inputStudent.nextLine();
 
         students.add(new Student(studentName, studentGrade, studentSubjects));
 
         System.out.println("\nStudent registration is: \n");
         for (Student student : students) {
-            student.printSchoolStudent(students.indexOf(student));
+            student.printStudent(students.indexOf(student));
+        }
+    }
+    private static void showOptionTwo() {
+        int menuConsult;
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.println("\nSelect an option");
+            System.out.println("1 - Query by grade");
+            System.out.println("2 - Query by subject");
+            menuConsult = input.nextInt();
+        }while (menuConsult < 1 || menuConsult > 2);
+        for (int i=0; i < students.size(); i++){
+            if (menuConsult == 1){
+                if(students.get(i).isGrade()){
+                    students.get(i).printGrade(i);
+                }
+                if (students.get(i).isGrade7()){
+                  students.get(i).printGrade(i);
+                }
+                if (students.get(i).isGrade8()){
+                    students.get(i).printGrade(i);
+                }
+                if (students.get(i).isGrade9()){
+                    students.get(i).printGrade(i);
+                }
+                if (students.get(i).isGrade10()){
+                    students.get(i).printGrade(i);
+                }
+                if (students.get(i).isGrade11()){
+                    students.get(i).printGrade(i);
+                }
+            }if (menuConsult == 2){
+                if (students.get(i).isSubject()){
+                    students.get(i).printSubject(i);
+                }
+                if (students.get(i).isSubject1()){
+                    students.get(i).printSubject(i);
+                }
+                if (students.get(i).isSubject2()){
+                    students.get(i).printSubject(i);
+                }
+            }
         }
     }
 
--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,74 @@
-
+import com.sun.org.apache.xerces.internal.impl.xs.util.StringListImpl;
+import com.sun.org.apache.xerces.internal.xs.StringList;
 
 public class Student {
-    String nameStudent;
-    String gradeEnter;
-    String matterRecorded;
+    String name;
+    String grade;
+    String subject;
 
-    public Student(String nameStudent, String gradeEnter, String matterRecorded){
-        this.nameStudent = nameStudent;
-        this.gradeEnter = gradeEnter;
-        this.matterRecorded = matterRecorded;
+    public Student(String name, String grade, String subject){
+        this.name = name;
+        this.grade = grade;
+        this.subject = subject;
     }
 
-    public void printSchoolStudent(int i){System.out.println(i+1+". Student's name "+this.nameStudent +" "+"grade you are entering is "+this.gradeEnter +" "+"your registered subjects are "+this.matterRecorded);}
+    public void printStudent(int i){System.out.println(i+1+". Student's name "+this.name +" "+"grade you are entering is "+this.grade +" "+"your registered subjects are "+this.subject);}
+    public void printGrade(int i){System.out.println(i+1+". Student's name "+this.name +" "+"grade you are entering is "+this.grade);}
+    public void printSubject(int i){System.out.println(i+1+". Student's name "+this.name +" "+"your registered subjects are "+this.subject);}
+
+    public boolean isGrade(){
+        if (grade.contains("sixth")){
+            return true;
+        }
+        return grade.contains("Sixth");
+    }
+    public boolean isGrade7(){
+        if (grade.contains("seventh")){
+            return true;
+        }
+        return grade.contains("Seventh");
+    }
+    public boolean isGrade8(){
+        if (grade.contains("eighth")){
+            return true;
+        }
+        return grade.contains("Eighth");
+    }
+    public boolean isGrade9(){
+        if (grade.contains("ninth")){
+            return true;
+        }
+        return grade.contains("Ninth");
+    }
+    public boolean isGrade10(){
+        if(grade.contains("tenth")){
+            return true;
+        }
+        return grade.contains("Tenth");
+    }
+    public boolean isGrade11(){
+        if (grade.contains("eleven")){
+            return true;
+        }
+        return grade.contains("Eleven");
+    }
+    public boolean isSubject(){
+        if (subject.contains("mathematics")){
+            return true;
+        }
+        return subject.contains("Mathematics");
+    }
+    public boolean isSubject1(){
+        if (subject.contains("spanish")){
+            return true;
+        }
+        return subject.contains("Spanish");
+    }
+    public boolean isSubject2(){
+        if (subject.contains("science")){
+            return true;
+        }
+        return subject.contains("Science");
+    }
+
 }
