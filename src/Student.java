9d7eb13bdaff02a1a893ccde5e08ddd93578b3b2
My AnFeVaGa <Job.andresvalencia@gmail.com>
2021-10-11 15:16:04 -0500

    School menu option two

--- src/Student.java
+++ src/Student.java
@@ -1,74 +1,49 @@
-import com.sun.org.apache.xerces.internal.impl.xs.util.StringListImpl;
-import com.sun.org.apache.xerces.internal.xs.StringList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Student {
     String name;
-    String grade;
-    String subject;
+    int grade;
+    ArrayList<String> subjects;
 
-    public Student(String name, String grade, String subject){
+    public Student(String name, int grade, ArrayList<String> subjects) {
         this.name = name;
         this.grade = grade;
-        this.subject = subject;
+        this.subjects = subjects;
+
     }
 
-    public void printStudent(int i){System.out.println(i+1+". Student's name "+this.name +" "+"grade you are entering is "+this.grade +" "+"your registered subjects are "+this.subject);}
-    public void printGrade(int i){System.out.println(i+1+". Student's name "+this.name +" "+"grade you are entering is "+this.grade);}
-    public void printSubject(int i){System.out.println(i+1+". Student's name "+this.name +" "+"your registered subjects are "+this.subject);}
 
-    public boolean isGrade(){
-        if (grade.contains("sixth")){
-            return true;
+    public void printStudent(int i)
+    {
+        System.out.println(i+1+". Student's name "+this.name +" "+"grade you are entering is " +
+            " " + " " +this.grade +" "+"your registered subjects are ");
+
+        for (String subjectString : subjects){
+            System.out.println(subjectString);
         }
-        return grade.contains("Sixth");
+
     }
-    public boolean isGrade7(){
-        if (grade.contains("seventh")){
-            return true;
-        }
-        return grade.contains("Seventh");
-    }
-    public boolean isGrade8(){
-        if (grade.contains("eighth")){
-            return true;
-        }
-        return grade.contains("Eighth");
-    }
-    public boolean isGrade9(){
-        if (grade.contains("ninth")){
-            return true;
-        }
-        return grade.contains("Ninth");
-    }
-    public boolean isGrade10(){
-        if(grade.contains("tenth")){
-            return true;
-        }
-        return grade.contains("Tenth");
-    }
-    public boolean isGrade11(){
-        if (grade.contains("eleven")){
-            return true;
-        }
-        return grade.contains("Eleven");
-    }
-    public boolean isSubject(){
-        if (subject.contains("mathematics")){
-            return true;
-        }
-        return subject.contains("Mathematics");
-    }
-    public boolean isSubject1(){
-        if (subject.contains("spanish")){
-            return true;
-        }
-        return subject.contains("Spanish");
-    }
-    public boolean isSubject2(){
-        if (subject.contains("science")){
-            return true;
-        }
-        return subject.contains("Science");
-    }
+
 
 }
+
+
+
+
+
+
+
+//    public boolean isGrade(){
+//        if (grade.contains("grade")){
+//            return true;
+//        }
+//        return grade.contains("Grade");
+//    }
+//
+//    public boolean isSubject(){
+//        if (subject.contains("mathematics")){
+//            return true;
+//        }
+//        return subject.contains("Mathematics");
+//    }
